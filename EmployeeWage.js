8bdbbd62fd5e7948a8ae3fbec64356ad89233685
Rohit Patro <rohitpatro6@gmail.com>
2021-08-27 00:53:24 +0530

    MODIFIED:Creation Of Employee Payroll Data Modified

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,45 +1,22 @@
-class EmployeePayrollData {
+class EmployeePayrollData{
     id;
     salary;
-    gender;
-    startDate;
 
-    //Constructor
-    constructor(...params) {
-        this.id = params[0];
-        this.name = params[1];
-        this.salary = params[2];
-        this.gender = params[3];
-        this.startDate = params[4];
+    constructor(id, name, salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
     }
 
-    // getter and setter
-    get name() {
-        return this._name;
-    }
-    set name(name) {
-        let nameRegex = RegExp('[A-Z]{1}[a-z]{2,}');
-        if (nameRegex.test(name))
-        this._name = name;
-        else throw "Name is Incorrect";
-    }
+    get name(){return this._name;}
+    set name(name){this._name = name;}
 
-    // Method
-    toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = this.startDate == undefined ? "undefined" : 
-        this.startDate.toLocaleDateString("en-In", options);
-        return "Id = " + this.id + " Name = " + this.name + " Salary = " + this.salary + " Gender = " + this.gender + " Start Date : " + empDate;
+    toString(){
+        return "id = "+ this.id + ", name = "+ this.name +", salary = "+this.salary;
     }
 }
 
-let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
+let employeePayrollData = new EmployeePayrollData(1, "Rohit", 100);
 console.log(employeePayrollData.toString());
-try {
-employeePayrollData.name = "John";
+employeePayrollData.name = "Rrp";
 console.log(employeePayrollData.toString());
-} catch (e) {
-        console.error(e);
-}
-let newEmployeePayrollData = new EmployeePayrollData(1, "Terisa", 30000, "F", new Date());
-console.log(newEmployeePayrollData.toString());
